7f3d488995952d7549cebc7d29086239c9d94003
Ben H <23144090+BenJayHutton@users.noreply.github.com>
2025-02-04 17:46:12 +0000

    initial commit

---
+++ sopranosomega/settings/production.py
@@ -0,0 +1,25 @@
+from .base import *
+import os
+from dotenv import load_dotenv 
+
+load_dotenv() 
+
+
+# Project settings
+ALLOWED_HOSTS=['.sopranosomega.com']
+DATABASE_URL = os.environ.get("DATABASE_URL", None)
+DEBUG = False
+DISABLE_COLLECTSTATIC = 1
+SECRET_KEY=os.environ.get("SECRET_KEY", None)
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get("MYSQL_DATABASE"),
+        'USER': os.environ.get("MYSQL_USERNAME"),
+        'PASSWORD': os.environ.get("MYSQL_PASSWORD"),
+        'HOST': 'localhost',
+    }
+}
+
+STATIC_ROOT = BASE_DIR.parent / "static"
